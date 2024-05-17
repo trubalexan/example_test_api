0f8f8912bcdbff988636b20e933f1fdd64079c2d
trubalexan <trubalexan@gmail.com>
2024-05-18 01:41:58 +0400

    added tests for user data format and time response limit

--- test_api/tests/test_scenarios.py
+++ test_api/tests/test_scenarios.py
@@ -1,5 +1,8 @@
 import copy
+import time
+
 from data.data_single_user import USER_ID_5
+from data.data_users import USERS_ANY
 from tests.test_users import check_users_api_success
 from tools.api_request_methods import request_endpoint
 from tools.create_random_data import create_id_list
@@ -98,7 +101,6 @@ def test_users_any_exists(testurl):
     :param testurl:
     :return:
     """
-    from data.data_users import USERS_ANY
     list_users_any = check_users_api_success(testurl, USERS_ANY).json()["idList"]
     any_user_test_data = copy.deepcopy(USER_ID_5)
     any_error_list = {}
@@ -118,7 +120,6 @@ def test_users_by_id(testurl):
     :param testurl:
     :return:
     """
-    from data.data_users import USERS_ANY
     list_users_by_id = check_users_api_success(testurl, USERS_ANY).json()["idList"]
     id_error_list = check_users_correct_id(testurl, list_users_by_id)
     assert not id_error_list, f"Test finished with errors: {id_error_list}"
@@ -157,7 +158,6 @@ def test_users_gender_any(testurl):
     :param testurl:
     :return:
     """
-    from data.data_users import USERS_ANY
     list_users_any = check_users_api_success(testurl, USERS_ANY).json()["idList"]
     any_error_list = check_users_list_gender(testurl, list_users_any, None)
     assert not any_error_list, f"Test finished with errors: {any_error_list}"
@@ -178,3 +178,103 @@ def test_all_ids(testurl):
             any_error_list[str(any_user)] = ex
     assert not any_error_list, f"Test finished with errors: {any_error_list}"
     print("Test finished with no errors")
+
+
+def test_data_format(testurl):
+    """
+    Test is checking that recieved user data has the proper format
+    """
+    user_format_data = copy.deepcopy(USER_ID_5)
+    list_users_id = check_users_api_success(testurl, USERS_ANY).json()["idList"]
+    format_error_list = {}
+    for user_id in list_users_id:
+        user_format_errors = {}
+        try:
+            user_format_data["link_extension"] = str(user_id)
+            user_format_answer = check_user_api(testurl, user_format_data)
+            data_format_json = user_format_answer.json()
+            print(data_format_json)
+            if data_format_json["isSuccess"] not in [True, False]:
+                user_format_errors["isSeccess"] = "wrong format"
+            if data_format_json["errorCode"] != 0:
+                user_format_errors["errorCode"] = "wrong code"
+            if data_format_json["errorMessage"] is not None:
+                user_format_errors["errorMessage"] = data_format_json["errorMessage"]
+            if not data_format_json["user"]:
+                user_format_errors["user"] = "no user data"
+            else:
+                if not isinstance(data_format_json["user"]["id"], int):
+                    user_format_errors["user_id"] = "wrong format"
+                if not isinstance(data_format_json["user"]["name"], str):
+                    user_format_errors["user_name"] = "wrong format"
+                if data_format_json["user"]["gender"] not in ["male", "female", "magic", "McCloud"]:
+                    user_format_errors["user_gender"] = "wrong format"
+                if isinstance(data_format_json["user"]["age"], int):
+                    if (data_format_json["user"]["age"] < 16) or (data_format_json["user"]["age"] > 100):
+                        user_format_errors["user_age"] = f'wrong age {data_format_json["user"]["age"]}'
+                else:
+                    user_format_errors["user_age"] = "wrong format"
+                if not isinstance(data_format_json["user"]["city"], str):
+                    user_format_errors["user_city"] = "wrong format"
+                reg_date_time_str = data_format_json["user"]["registrationDate"]
+                # if len(reg_date_time_str) > 23:
+                #     print(reg_date_time_str)
+                #     reg_date_time_str = reg_date_time_str[:23]
+                #     print(reg_date_time_str)
+                # reg_len = len(reg_date_time_str)
+                if len(reg_date_time_str) < 19:
+                    user_format_errors["user_registrationDate"] = f"wrong format for {reg_date_time_str}"
+                else:
+                    if reg_date_time_str[4] == "-" and reg_date_time_str[7] == "-" and reg_date_time_str[10] == "T" \
+                        and reg_date_time_str[13] == ":" and reg_date_time_str[16] == ":": # and reg_date_time_str[
+                            # 19] == ".":
+                        reg_year = int(reg_date_time_str[:4])
+                        if (2000 > reg_year) or (reg_year > 2024):
+                            user_format_errors["user_reg_year"] = f"wrong year {reg_year}"
+                        reg_month = int(reg_date_time_str[5:7])
+                        if (reg_month <= 0) or (reg_month > 12):
+                            user_format_errors["user_reg_month"] = f"wrong month {reg_month}"
+                        reg_day = int(reg_date_time_str[8:10])
+                        if (reg_day <= 0) or (reg_day > 31):
+                            user_format_errors["user_reg_day"] = f"wrong day {reg_day}"
+                        reg_hour = int(reg_date_time_str[11:13])
+                        if (reg_hour < 0) or (reg_hour > 24):
+                            user_format_errors["user_reg_hour"] = f"wrong hours {reg_hour}"
+                        reg_minute = int(reg_date_time_str[14:16])
+                        if (reg_minute < 0) or (reg_minute > 59):
+                            user_format_errors["user_reg_minute"] = f"wrong minutes {reg_minute}"
+                        reg_seconds = float(reg_date_time_str[17:])
+                        if (reg_seconds < 0) or (reg_seconds > 59.999):
+                            user_format_errors["user_registrationDate"] = f"wrong seconds {reg_seconds}"
+                    else:
+                        user_format_errors["user_registrationDate"] = f"wrong format {reg_date_time_str}"
+            assert not user_format_errors
+        except Exception as ex:
+            format_error_list[str(user_id)] = ex
+            # print(ex)
+    assert not format_error_list, f"Test finished with errors: {format_error_list}"
+    print("Test finished with no errors")
+
+
+def test_response_time(testurl):
+    """
+    Test is checking response time limit = 1 sec
+    """
+    user_format_data = copy.deepcopy(USER_ID_5)
+    list_users_id = check_users_api_success(testurl, USERS_ANY).json()["idList"]
+    time_limit_error_list = {}
+    response_time_list = {}
+    for user_id in list_users_id:
+        try:
+            user_format_data["link_extension"] = str(user_id)
+            start_time = time.time_ns()
+            check_user_api(testurl, user_format_data)
+            stop_time = time.time_ns()
+            response_time_list[user_id] = (stop_time - start_time) * 0.000000001
+            if response_time_list[user_id] > 1:
+                time_limit_error_list[user_id] = response_time_list[user_id]
+        except Exception as ex:
+            time_limit_error_list[str(user_id)] = ex
+            # print(ex)
+    print("время отклика:\t", response_time_list)
+    assert not time_limit_error_list, f"Test finished with errors: {time_limit_error_list}"
